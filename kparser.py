a68a3c4f159b4c3492409c2b5e71e9c084ca6e63
rkorzen <korzeniewski@gmail.com>
2014-12-29 16:30:01 +0100

    Parsowanie blokow i stron - drzewo

--- kparser.py
+++ kparser.py
@@ -1,10 +1,7 @@
 __author__ = 'korzen'
-from BeautifulSoup import BeautifulSoup
+from bs4 import BeautifulSoup
+#from BeautifulSoup4 import BeautifulSoup
 from collections import OrderedDict
-
-
-
-last_block =  None
 
 class Survey():
     def __init__(self):
@@ -30,10 +27,11 @@ class Survey():
                 pass
         return out
 
+
 class Block():
-    def __init__(self):
-        self.parent = None
-        self.id = None
+    def __init__(self, id, parent=None):
+        self.parent = parent
+        self.id = id
         self.childs = OrderedDict()
 
     def add_child(self, child):
@@ -45,22 +43,36 @@ class Block():
         else:
             return False
 
-def print_tree_2(block, level=0):
-    out = ''
-    if block.has_child():
-        level += 1
-        for key in block.childs:
-            out +='\t'*level + key + 'lvl: ' + str(level) +'\n'
-            out += print_tree_2(block.childs[key], level)
-    else:
-        level = 0
-    return out
+    def __str__(self):
+        return self.id
 
-class Page():
-    def __init__(self, id=None, parent=None):
-        self.id = id
-        self.parent = parent
-        self.childs = OrderedDict()
+    def __unicode__(self):
+        return self.id
+
+
+    def childs_tree(self, level=0):
+
+        out = ''
+        for child in self.childs:
+            #print("jestem w pętli for, level = ", level, 'child = ', child)
+            out += '\t' * level + child + ' lvl: ' + str(level) +'\n'
+            if len(self.childs[child].childs) > 0:
+                level += 1
+                out += self.childs[child].childs_tree(level)
+                level -= 1
+
+        return out
+
+    def add_child_to_parent(self, b):
+        for child in self.childs:
+            if child == b.parent:
+                #print("dodaję do: {0} dziecko {1}".format(child, b))
+                self.childs[child].add_child(b)
+            else:
+                self.childs[child].add_child_to_parent(b)
+
+
+class Page(Block):
 
     def add_question(self, q):
         self.childs[q.id] = q
@@ -77,59 +89,61 @@ class Question(object):
 
 
 def parser(text):
-    survey = Block()
+    survey = Block('survey')
     text = text.splitlines()
+    current_block = None
+
     for line in text:
         if line.startswith('B '):
-            line_tmp = line.split(' ')
-            id = line_tmp[1]
-            b = Block()
-            b.id = id
-            b.typ = "block"
+            line = line.split()
+
+            block_id = line[1]
+
             try:
-                parent = line_tmp[2]
-                last_block.add_child(b)
+                block_parent_id = line[2]
             except IndexError:
+                block_parent_id = None
+
+            b = Block(block_id, block_parent_id)
+            if b.parent:
+                survey.add_child_to_parent(b)
+            else:
                 survey.add_child(b)
 
-            last_block = b
+            current_block = b
+            continue
 
         if line.startswith('P '):
-            line_tmp = line.split(' ')
-            id = line_tmp[1]
-            p = Page(id, last_block.id)
-            last_block.add_child(p)
-            last_page_id = id
+            line = line.split()
+            page_id = line[1]
+            p = Page(page_id, current_block.id)
 
-        if line.startswith('Q '):
-            line_tmp = line.split(' ')
-            id = line_tmp[2]
-            typ = line_tmp[1]
-            polecenie = line_tmp[3]
-            q = Question(id, typ, polecenie)
-            try:
-                last_block.childs[last_page_id].add_question(q)
-            except:
-                # musze strone utworzyc:
-                page_id = q.id + '_p'
-                p = Page(page_id)
-                p.add_question(q)
-                last_block.add_child(p)
+            current_block.add_child(p)
+
+            continue
+        # if line.startswith('P '):
+        #     line_tmp = line.split(' ')
+        #     id = line_tmp[1]
+        #     p = Page(id, last_block.id)
+        #     last_block.add_child(p)
+        #     last_page_id = id
+        #
+        # if line.startswith('Q '):
+        #     line_tmp = line.split(' ')
+        #     id = line_tmp[2]
+        #     typ = line_tmp[1]
+        #     polecenie = line_tmp[3]
+        #     q = Question(id, typ, polecenie)
+        #     try:
+        #         last_block.childs[last_page_id].add_question(q)
+        #     except:
+        #         # musze strone utworzyc:
+        #         page_id = q.id + '_p'
+        #         p = Page(page_id)
+        #         p.add_question(q)
+        #         last_block.add_child(p)
     return survey
     #print(elements)
-
-
-def check_tree_level(elements, parent_id, level):
-
-    if parent_id is not None:
-        level += 1 + check_tree_level(elements, elements[parent_id].parent, level)
-    else:
-        level = 0
-    return level
-
-
-
-
 
 
 if __name__ == "__main__":
@@ -153,10 +167,29 @@ B B6 B5
 """
     text = '''B B1
 B B2 B1
+P P2
 B B3 B1
 B B4 B1
 B B5
-'''
+B B6
+B B7 B6
+B B8 B7
+B B9 B8
+B B10
+B B11 B10
+P P1'''
+
+#     text = '''B B1
+# B B2 B1
+# P P2
+# B B3 B1
+# B B4 B1
+# '''
+
+
+
 
     s = parser(text)
-    print(print_tree_2(s))
+    #print(s.childs)
+    #print(s.childs['B1'].childs)
+    print(s.childs_tree())
