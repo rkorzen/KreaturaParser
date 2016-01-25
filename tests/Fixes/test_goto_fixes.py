41307f37f7c587c2e68b764336c68b0bd9c40094
rkorzen <korzeniewski@gmail.com>
2016-01-25 16:38:49 +0100

    Fix4

--- tests/Fixes/test_goto_fixes.py
+++ tests/Fixes/test_goto_fixes.py
@@ -157,3 +157,153 @@ endif]]>
                     </survey>'''.format(survey.createtime)
 
         self.assertXmlEqual(got, want)
+
+
+class TestFix4(KreaturaTestCase):
+    def test_sliders(self):
+        input_ = """Q SLIDERS Q1 COS
+cokolwiek
+_
+01 A
+02 B
+03 C"""
+
+        survey = parse(input_)
+        survey.to_xml()
+        print(survey.createtime)
+
+        got = etree.tostring(survey.xml)
+        want = '''<survey SMSComp="false"
+                          createtime="{0}"
+                          creator="CHANGEIT"
+                          exitpage=""
+                          layoutid="ShadesOfGray"
+                          localeCode="pl"
+                          name="CHANGEIT"
+                          sensitive="false"
+                          showbar="false"
+                          time="60000">
+                          <block id="Default"
+                                 quoted="false"
+                                 random="false"
+                                 rotation="false"
+                                 name="">
+  <page id="Q1_p" hideBackButton="false">
+    <precode></precode>
+    <postcode></postcode>
+    <question id="Q1_p.Q1" name="">
+<control_layout id="Q1_p.Q1.label1" layout="default" style="">
+<content>COS</content>
+</control_layout>
+<control_table id="Q1_p.Q1.table1" random="false" rotation="true" rrdest="row" style="">
+
+    <row forcestable="false" style="">
+    <cell colspan="1" forcestable="true" rowspan="1" style="">
+    <control_layout id="Q1_1_txt0" layout="default" style="">
+    <content>01 A</content>
+    </control_layout>
+    </cell>
+    <cell colspan="1" forcestable="true" rowspan="1" style="">
+    <control_layout id="Q1_1_txt1" layout="default" style="">
+    <content>Lewy koniec</content>
+    </control_layout>
+    </cell>
+    <cell colspan="1" forcestable="false" rowspan="1" style="">
+    <control_number float="false" id="Q1_1" mask=".*" name="Q1_1 01 A" require="true" results="true" style="">
+    <content/>
+    </control_number>
+    </cell>
+    <cell colspan="1" forcestable="false" rowspan="1" style="">
+    <control_layout id="Q1_1_txt2" layout="default" style="">
+    <content>Prawy koniec</content>
+    </control_layout>
+    </cell>
+    </row>
+
+    <row forcestable="false" style="">
+    <cell colspan="1" forcestable="true" rowspan="1" style="">
+    <control_layout id="Q1_2_txt0" layout="default" style="">
+    <content>02 B</content>
+    </control_layout>
+    </cell>
+    <cell colspan="1" forcestable="true" rowspan="1" style="">
+    <control_layout id="Q1_2_txt1" layout="default" style="">
+    <content>Lewy koniec</content>
+    </control_layout>
+    </cell>
+    <cell colspan="1" forcestable="false" rowspan="1" style="">
+    <control_number float="false" id="Q1_2" mask=".*" name="Q1_2 02 B" require="true" results="true" style="">
+    <content/>
+    </control_number>
+    </cell>
+    <cell colspan="1" forcestable="false" rowspan="1" style="">
+    <control_layout id="Q1_2_txt2" layout="default" style="">
+    <content>Prawy koniec</content>
+    </control_layout>
+    </cell>
+    </row>
+
+    <row forcestable="false" style="">
+    <cell colspan="1" forcestable="true" rowspan="1" style="">
+    <control_layout id="Q1_3_txt0" layout="default" style="">
+    <content>03 C</content>
+    </control_layout>
+    </cell>
+    <cell colspan="1" forcestable="true" rowspan="1" style="">
+    <control_layout id="Q1_3_txt1" layout="default" style="">
+    <content>Lewy koniec</content>
+    </control_layout>
+    </cell>
+    <cell colspan="1" forcestable="false" rowspan="1" style="">
+    <control_number float="false" id="Q1_3" mask=".*" name="Q1_3 03 C" require="true" results="true" style="">
+    <content/>
+    </control_number>
+    </cell>
+    <cell colspan="1" forcestable="false" rowspan="1" style="">
+    <control_layout id="Q1_3_txt2" layout="default" style="">
+    <content>Prawy koniec</content>
+    </control_layout>
+    </cell>
+    </row>
+
+</control_table>
+<control_layout id="Q1_p.Q1.label2" layout="default" style="">
+<content>&lt;link rel="stylesheet" href="public/slider/css/ui-lightness/jquery-ui-1.8.9.custom.css" type="text/css"&gt;
+&lt;link rel="stylesheet" href="public/slider/slider.css" type="text/css"&gt;
+&lt;link rel="stylesheet" href="public/custom.css" type="text/css"&gt;
+
+&lt;script type='text/javascript' src='public/slider/js/jquery-ui-1.8.9.custom.min.js'&gt;&lt;/script&gt;
+
+&lt;script type='text/javascript' src='public/slider/slider.js'&gt;&lt;/script&gt;
+&lt;script type='text/javascript'&gt;
+     sliderOpts = {
+          value: 0,
+          min: 1,
+          max: 5,
+          step: 1,
+          animate:"slow",
+          orientation: 'horizontal'
+     };
+new IbisSlider("Q1_1", sliderOpts);new IbisSlider("Q1_2", sliderOpts);new IbisSlider("Q1_3", sliderOpts);
+&lt;/script&gt;</content>
+</control_layout>
+</question>
+  </page>
+
+
+                          </block>
+                        <vars/>
+                        <procedures>
+                        <procedure id="PROC" shortdesc=""/>
+                        </procedures>
+                    </survey>'''.format(survey.createtime)
+        self.assertXmlEqual(got, want)
+
+
+    def test_sliders_with_hide(self):
+        input_ = """Q SLIDERS Q1 COS
+cokolwiek
+_
+01 A --hide:$Z4a:{0}=="1"
+02 B
+03 C"""
